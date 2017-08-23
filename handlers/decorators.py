54a408f6b75627b93fd271f1c341ef9feeb74a92
lov3catch <lov3catch@gmail.com>
2017-08-24 00:57:37 +0400

    add updated and created datetime

--- handlers/decorators.py
+++ handlers/decorators.py
@@ -12,16 +12,20 @@ def save_chanel_decorator(fn):
     def wrapper(bot, update, *args, **kwargs):
         print('SAVE CHANEL')
 
+        print(update.callback_query)
+
         try:
             if(update.callback_query):
+                chat_id = update.callback_query.message.chat.id
                 first_name = update.callback_query.message.chat.first_name
                 last_name = update.callback_query.message.chat.last_name
             else:
+                chat_id = update.message.chat.id
                 first_name = update.message.chat.first_name
                 last_name = update.message.chat.last_name
 
             defaults = {'chanel_id': update.message.chat.id, 'first_name': first_name, 'last_name': last_name}
-            chanel, is_new = Chanel.get_or_create(chanel_id=update.message.chat.id, defaults=defaults)
+            chanel, is_new = Chanel.get_or_create(chanel_id=chat_id.id, defaults=defaults)
             chanel.save(True)
         except Exception as ex:
             print(ex)

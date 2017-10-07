c96fbe641183fff784ca837b28306c1f4f5ebec2
lov3catch <lov3catch@gmail.com>
2017-10-07 23:37:29 +0400

    исправил сообщения

--- core/handlers/messages.py
+++ core/handlers/messages.py
@@ -1,26 +1,36 @@
-import os
-from functools import partial
-
-MESSAGES = {
-    'RU': {
-        'intro': 'Самое время для хорошей музыки. Я уже готов искать твои любимые треки, с чего начнём?',
-        'searching': 'Ищу...',
-        'i_find': 'Смотри, что я нашел:',
-        'i_try': 'Я действительно старался, но нет ничего :('
-    },
-    'EN': {
-        'intro': 'It`s time for good music. I`m ready to search for your favorite tracks, where do we start?',
-        'searching': 'Searching...',
-        'i_find': 'Check this sounds, bro:',
-        'i_try': 'I really tried but i can`t find anything :('
+class Messages:
+    ALL = {
+        'ru': {
+            'intro': 'Самое время для хорошей музыки. Я уже готов искать твои любимые треки, с чего начнём?',
+            'searching': 'Ищу...',
+            'i_find': 'Смотри, что я нашел:',
+            'i_try': 'Я действительно старался, но нет ничего :('
+        },
+        'en': {
+            'intro': 'It`s time for good music. I`m ready to search for your favorite tracks, where do we start?',
+            'searching': 'Searching...',
+            'i_find': 'Check this sounds, bro:',
+            'i_try': 'I really tried but i can`t find anything :('
+        }
     }
-}
+    DEFAULT_LANGUAGE = 'RU'
 
-selected_language = os.getenv('LANGUAGE', 'EN')
+    language = DEFAULT_LANGUAGE
+
+    def set_language(self, language):
+        accepted_language = ('ru', 'en')
+        language = language.lower()
+        self.language = language if language in accepted_language else self.DEFAULT_LANGUAGE
+
+    def get_massage(self, alias):
+        return self.ALL[self.language][alias]
 
 
-def get_message(language, messages, key):
-    return messages[language][key]
+if __name__ == '__main__':
+    language_processor = Messages()
 
+    language_processor.set_language('ru')
+    print(language_processor.get_massage('i_find'))
 
-get_message_by_key = partial(partial(get_message, selected_language), MESSAGES)
+    language_processor.set_language('en')
+    print(language_processor.get_massage('i_find'))

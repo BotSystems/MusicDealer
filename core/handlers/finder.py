81f1d1e3dc4b6a25ddb55492306c3fb7ad0ce180
lov3catch <lov3catch@gmail.com>
2017-11-15 00:53:35 +0300

    убрал парсинг

--- core/handlers/finder.py
+++ core/handlers/finder.py
@@ -6,8 +6,8 @@ import requests
 
 from core.handlers.audio_parsers import prepare_result
 
-SEARCH_URL = os.getenv('SEARCH_TRACKS_TEMPLATE', 'http://127.0.0.1:5000/search?query={}')
-DOWNLOAD_URL = os.getenv('DOWNLOAD_TRACK_TEMPLATE', )
+SEARCH_URL = os.getenv('SEARCH_TRACKS_TEMPLATE')
+DOWNLOAD_URL = os.getenv('DOWNLOAD_TRACK_TEMPLATE')
 
 
 def normalize_song_name(song_name):
@@ -23,16 +23,16 @@ def parse_result(normalized_song_name):
 
 
 def normalize_download_url(data_url):
-    url = urljoin(DOWNLOAD_URL, data_url)
+    url = DOWNLOAD_URL.format(data_url)
     result = requests.get(url).json()
-    url = dict(result).get('url')
-    if url:
-        url = str.split(url, '?')
-    if url:
-        return url[0]
+    if 'data' in result:
+        return result['data']['download_url']
     return None
 
 
 if __name__ == '__main__':
     data_urls = parse_result(normalize_song_name('The Hardkiss'))
     print(data_urls)
+
+    download_url = normalize_download_url("/musicset/play/936394cc1b6ac6e01ea123f96033bd8a/3946953.json")
+    print(download_url)

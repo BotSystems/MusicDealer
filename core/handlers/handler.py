28d7074ed5949cc810a11b2c9d51fdc457d9f086
lov3catch <lov3catch@gmail.com>
2017-11-26 16:01:54 +0300

    disable saving download

--- core/handlers/handler.py
+++ core/handlers/handler.py
@@ -95,7 +95,9 @@ def searching(bot, chat_id, text, limit, offset):
 
 
 def next_page(bot, update, *args, **kwargs):
-    bot.editMessageText('aaaaaaaaaaaaaa', update.callback_query.message.chat_id, update.callback_query.message_id)
+    print(update.callback_query.message.chat_id)
+    print(update.callback_query.message_id)
+    bot.edit_message_text('aaaaaaaaaaaaaa', update.callback_query.message.chat_id, update.callback_query.message_id)
     # query = update.callback_query
     # print(query)
     # limit = int(query.data.split('.')[3])

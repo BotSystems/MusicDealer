0724d8928acc429cf9ad429307222f60700c0fb8
lov3catch <lov3catch@gmail.com>
2018-01-21 23:42:29 +0300

    add debug info

--- core/bot/factory.py
+++ core/bot/factory.py
@@ -1,4 +1,5 @@
 from telegram.ext import Dispatcher
+from telegram.ext import messagequeue as mq
 
 from core.bot.models import Bot
 from core.handlers.handler import init_handlers
@@ -8,7 +9,11 @@ class Factory:
     @staticmethod
     def create(area):
         bot = Bot(area.token)
+
         bot.area = area
         bot.dispatcher = init_handlers(Dispatcher(bot, None))
+
+        bot._is_messages_queued_default = True
+        bot._msg_queue = mq.MessageQueue(25)
 
         return bot
--- core/bot/models.py
+++ core/bot/models.py
@@ -6,13 +6,8 @@ class Bot(telegram.Bot):
     area = None
     dispatcher = None
 
-    '''A subclass of Bot which delegates send method handling to MQ'''
-
-    def __init__(self, *args, is_queued_def=True, mqueue=None, **kwargs):
-        super(Bot, self).__init__(*args, **kwargs)
-        # below 2 attributes should be provided for decorator usage
-        self._is_messages_queued_default = is_queued_def
-        self._msg_queue = mqueue or mq.MessageQueue(25)
+    _is_messages_queued_default = None
+    _msg_queue = None
 
     def __del__(self):
         try:

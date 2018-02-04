17cf95f8202f081242004640f4cb3506b23def50
lov3catch <lov3catch@gmail.com>
2018-02-04 14:49:02 +0300

    add commands

--- core/bot/models.py
+++ core/bot/models.py
@@ -6,22 +6,22 @@ class Bot(telegram.Bot):
     area = None
     dispatcher = None
 
-    _is_messages_queued_default = None
-    _msg_queue = None
-
-    def __del__(self):
-        try:
-            self._msg_queue.stop()
-        except:
-            pass
-        super(Bot, self).__del__()
-
-    @mq.queuedmessage
-    def forward_message(self,
-                        chat_id,
-                        from_chat_id,
-                        message_id,
-                        disable_notification=False,
-                        timeout=None,
-                        **kwargs):
-        return super(Bot, self).forward_message(chat_id, from_chat_id, message_id, disable_notification, timeout, **kwargs)
+    # _is_messages_queued_default = None
+    # _msg_queue = None
+    #
+    # def __del__(self):
+    #     try:
+    #         self._msg_queue.stop()
+    #     except:
+    #         pass
+    #     super(Bot, self).__del__()
+    #
+    # @mq.queuedmessage
+    # def forward_message(self,
+    #                     chat_id,
+    #                     from_chat_id,
+    #                     message_id,
+    #                     disable_notification=False,
+    #                     timeout=None,
+    #                     **kwargs):
+    #     return super(Bot, self).forward_message(chat_id, from_chat_id, message_id, disable_notification, timeout, **kwargs)

5026cb1f909eb5a74e7ff67114ea1fcd7f528c12
lov3catch <lov3catch@gmail.com>
2018-02-11 15:33:51 +0300

    add commands

--- core/handlers/handler.py
+++ core/handlers/handler.py
@@ -105,7 +105,7 @@ def broadcast(bot, update):
 def is_group_available_for_broadcast(bot, update, callback):
     # print(update)
     # print(update.channel_post.chat.username)
-    available_groups = os.getenv('AVAILABLE_CHANNELS', '').split(',')
+    available_groups = os.getenv('AVAILABLE_CHANNELS', '')
     print(':::::', available_groups)
 
 def handle_message(bot, update):

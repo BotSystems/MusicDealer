e49d30be8580d71744aa7361b73d0d01e22e9783
lov3catch <online777>
2018-11-11 18:03:53 +0200

    no storage

--- core/amqp.py
+++ core/amqp.py
@@ -7,19 +7,20 @@ CLOUDAMQP_URL = os.getenv('CLOUDAMQP_URL')
 QUEUE = os.getenv('QUEUE_NAME')
 
 def upload_to_queue(download_url):
-    if (2 == random.randint(1, 2)):
-        parameters = pika.URLParameters(CLOUDAMQP_URL)
-        connection = pika.BlockingConnection(parameters)
-
-        channel = connection.channel()
-        data = {'payload': {'url': download_url}}
-
-        try:
-            channel.basic_publish('', QUEUE, json.dumps(data), pika.BasicProperties(content_type='text/plain', delivery_mode=1))
-        except Exception as ex:
-            print(ex.message)
-        finally:
-            connection.close()
+    pass
+    # if (2 == random.randint(1, 2)):
+    #     parameters = pika.URLParameters(CLOUDAMQP_URL)
+    #     connection = pika.BlockingConnection(parameters)
+    #
+    #     channel = connection.channel()
+    #     data = {'payload': {'url': download_url}}
+    #
+    #     try:
+    #         channel.basic_publish('', QUEUE, json.dumps(data), pika.BasicProperties(content_type='text/plain', delivery_mode=1))
+    #     except Exception as ex:
+    #         print(ex.message)
+    #     finally:
+    #         connection.close()
 
 if __name__ == '__main__':
     upload_to_queue('https://example.com')

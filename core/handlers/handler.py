35a214aa0f4c57983bc0ba608862309bb3836a29
igor.kpl <lov3catch@gmail.com>
2019-05-14 21:51:41 +0300

    add debug

--- core/handlers/handler.py
+++ core/handlers/handler.py
@@ -227,6 +227,7 @@ def download_song(bot, update, *args, **kwargs):
 
     download_url = normalize_download_url(track_link, provider)
     print('DOWNLOAD-URL: ', download_url)
+    print('----------', query.message.chat_id)
     # upload_to_queue(download_url)
     bot.send_audio(query.message.chat_id, download_url)
 
